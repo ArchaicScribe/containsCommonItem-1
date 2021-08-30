9c3dc81522ef1a64f9c26f6988db2f8572d1e2f3
Alex Rauenzahn <xandermv@yahoo.com>
2021-08-30 22:06:32 +0000

    Created a better version. O(a+b) time. It can still be improved more.

--- index.js
+++ index.js
@@ -12,21 +12,56 @@
 //returns true/false
 //O(n^2)
 
-const array1= ['a','b','c','x'];
-const array2= ['z','y','i'];
 
 
-function findCommonItem(arr1, arr2){
-	for (let i= 0; i < arr1.length; i++){
-		for(let j = 0; j < arr2.length; j++){
-			if(arr1[i] === arr2[j]){
-				return true;
-			}
-		}
-	}
-	return false; 
-}
+
+// function findCommonItem(arr1, arr2){
+// 	for (let i= 0; i < arr1.length; i++){
+// 		for(let j = 0; j < arr2.length; j++){
+// 			if(arr1[i] === arr2[j]){
+// 				return true;
+// 			}
+// 		}
+// 	}
+// 	return false; 
+// }
 
 //O(a * b) Not the best time
 
-findCommonItem(array1, array2);
+// findCommonItem(array1, array2);
+
+
+const array1= ['a','b','c','x'];
+const array2= ['z','y','x'];
+
+//array ==> obj{
+//a: true,
+//b: true,
+//c: true,
+//d: true
+//}
+//array[index] === obj.properties
+
+//Can we always assume two parameters in the function?
+
+function containsCommonItem2(arr1, arr2){
+	//loop throught first array and create object where properties ==== items in the array
+	let map = {};
+	for(let i = 0; i < arr1.length; i++){
+		if(!map[arr1[i]]){
+			const item = arr1[i];
+			map[item] = true;
+		}
+	}
+	//loop through second array and check if item in second array exists on the created object. This will improve performance. 
+	for(let j = 0; j < arr2.length; j++){
+		if(map[arr2[j]]){
+			return true;
+		}
+	}
+	return false;
+}
+
+containsCommonItem2(array1, array2)
+
+//O(a + b) Time Complexity is better than O(a *b)
